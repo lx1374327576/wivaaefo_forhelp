f16e7bf92db6dabf88f1cbffc26c05ccf842764b
ztzhenshuai <42974841+ztzhenshuai@users.noreply.github.com>
2020-06-05 18:11:14 +0800

    哈工大

--- query.py
+++ query.py
@@ -10,8 +10,8 @@ password: 密码
 import sqlite3 as sql
 con = sql.connect('database.db')
 cur = con.cursor()
-# cur.execute('insert into secret_list values(?, ?, ?, ?, ?)',
-#             (6, '中南大学', 'http://www.myszu.cn', 'LeetCodeKing', 'suanfa!1234'))
+cur.execute('insert into secret_list values(?, ?, ?, ?, ?)',
+            (7, '哈工大', 'http://www.myhit.cn', 'LeetCodeKing', 'suanfa!1234'))
 # cur.execute('update secret_list set bbs_url="http://bbs.csu.edu.cn/bbs/" where id=6')
 con.commit()
 print(cur.execute('select * from secret_list').fetchall())
