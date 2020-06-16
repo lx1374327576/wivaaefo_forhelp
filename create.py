345f8f7949b243e35d153c571b1a0b23f33c7310
ztzhenshuai <42974841+ztzhenshuai@users.noreply.github.com>
2020-06-16 15:50:01 +0800

    数据库

--- create.py
+++ create.py
@@ -1,6 +1,9 @@
 import sqlite3 as sql
 con = sql.connect('database.db')
 cur = con.cursor()
-con.execute('CREATE TABLE IF NOT EXISTS secret_list(id primary key, school_name, bbs_url, user_name, password)')
+con.execute('''CREATE TABLE IF NOT EXISTS work_list
+(id INTEGER primary key,
+name_id REFERENCES person_list(id) ,
+company_name,station,choice,program,price)''')
 cur.close()
 con.close()
--- query.py
+++ query.py
@@ -10,10 +10,10 @@ password: 密码
 import sqlite3 as sql
 con = sql.connect('database.db')
 cur = con.cursor()
-cur.execute('insert into secret_list values(?, ?, ?, ?, ?)',
-            (7, '哈工大', 'http://www.myhit.cn', 'LeetCodeKing', 'suanfa!1234'))
+cur.execute('insert into work_list values(?, ?, ?, ?, ?,?,?)',
+            (2,2,'哈工大', 'http://www.myhit.cn', 'LeetCodeKing', 'suanfa!1234',546))
 # cur.execute('update secret_list set bbs_url="http://bbs.csu.edu.cn/bbs/" where id=6')
 con.commit()
-print(cur.execute('select * from secret_list').fetchall())
+print(cur.execute('select * from work_list').fetchall())
 cur.close()
 con.close()
